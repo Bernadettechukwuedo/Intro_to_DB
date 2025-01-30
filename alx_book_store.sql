d8bdaca5f891c6e236a1ea89544f129702c16576
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-01-30 09:52:29 +0100

    Updated the file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,13 +25,13 @@ CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ) --CREATE ORDER DETAILS TABLE
 CREATE TABLE Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
 )
